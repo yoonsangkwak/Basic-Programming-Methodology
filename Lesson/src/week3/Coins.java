20df1eeecda641d4a5e7cd259d36cb1ca5c35671
yoonsangkwak <yunsang08@naver.com>
2020-09-21 18:12:37 +0900

    coins

---
+++ Lesson/src/week3/Coins.java
@@ -0,0 +1,9 @@
+package week3;
+
+public class Coins {
+    public static void main(String[] args) {
+        System.out.println("For 9 quarters, 2 dimes, no nickles, and 6 pennies, ");
+        System.out.print("the total is ");
+        System.out.println( (9 * 25) + (2 * 10) + (0 * 5) + (6 * 1));
+    }
+}
